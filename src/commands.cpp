df68fe39e649c29277006d18375e56311f14d40f
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-12-21 16:17:16 +0530

    feat: running external programs
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- src/commands.cpp
+++ src/commands.cpp
@@ -1,13 +1,15 @@
 #include<bits/stdc++.h>
 #include"headers/commands.hpp"
+#include<unistd.h>
+#include <sys/wait.h>
 using namespace std;
 
 vector<string> shell_builtin_commands={"echo","type","exit"};
 
-bool is_executable(const string &token) {
+string is_executable(const string &token) {
     const char* path = getenv("PATH");
     if (!path) {
-        return false;
+        return "";  // PATH not set
     }
     string path_str = path;
     stringstream ss(path_str);
@@ -15,11 +17,31 @@ bool is_executable(const string &token) {
     while (getline(ss, dir, ':')) {
         string full_path = dir + "/" + token;
         if (access(full_path.c_str(), X_OK) == 0) {  // Check if executable
-            cout << token << " is " << full_path << endl;
-            return true;
+            return full_path;
         }
     }
-    return false;
+    return "";
+}
+
+void execute_executables(const string &exec_path, const vector<string> &tokens) {
+    pid_t pid = fork();
+    if (pid == 0) {  // Child process
+        vector<char*> args;
+        for (const auto& token : tokens) {
+            args.push_back(const_cast<char*>(token.c_str()));
+        }
+        args.push_back(nullptr);
+        execv(exec_path.c_str(), args.data());
+        // If execv returns, an error occurred
+        cerr << "Failed to execute " << exec_path << endl;
+        exit(1);
+    } else if (pid > 0) {  // Parent process
+        int status;
+        waitpid(pid, &status, 0);
+    } else {
+        cerr << "Fork failed" << endl;
+    }
+
 }
 
 void handleCommand(const std::vector<std::string>& tokens) {
@@ -30,6 +52,11 @@ void handleCommand(const std::vector<std::string>& tokens) {
         handleType(tokens);
     }
      else {
+        string exec_path=is_executable(tokens[0]);
+        if(!exec_path.empty()){
+           execute_executables(exec_path,tokens);
+            return;
+        }
         cout << tokens[0] << ": command not found" << endl;
     }
 }
@@ -53,10 +80,13 @@ void handleType(const std::vector<std::string>& tokens) {
     const string token=tokens[1];
     if(shell_builtin_commands.end()!=find(shell_builtin_commands.begin(),shell_builtin_commands.end(),token)){
         cout<<token<<" is a shell builtin"<<endl;
-    }else if(is_executable(token)){
-        // is_executable already prints the path
-    }else{
-        cout<<token<<": not found" << endl;
+    }else {
+        string exec_path=is_executable(token);
+        if(!exec_path.empty()){
+            cout<<token<<" is "<<exec_path<<endl;
+        }else{
+            cout<<token<<": not found"<<endl;
+        }
     }
 
 }
