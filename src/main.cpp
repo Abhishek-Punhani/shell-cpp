c2cbe4fb41b98c4e0b18bff5b0430fb3e9ff69f2
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-12-21 14:54:06 +0530

    adding exit
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
-#include <iostream>
-#include <string>
-
+#include<bits/stdc++.h>
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << std::unitbuf;
+  cerr << std::unitbuf;
   while (true)
   {
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << input << ": command not found" << std::endl;
+    cout << "$ ";
+    string input;
+    getline(cin, input);
+    if (input == "exit") {
+      break;
+    }
+    cout << input << ": command not found" << endl;
 
   }
   
