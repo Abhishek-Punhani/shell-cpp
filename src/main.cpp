6664f1245d98e175f8be4941ff4e2cb7aff5e9af
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-12-21 14:52:59 +0530

    intiialsiing repl
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,13 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  while (true)
+  {
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found" << std::endl;
 
-  std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
-}
+  }
+  
+  }
