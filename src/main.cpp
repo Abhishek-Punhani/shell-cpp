35a993e0bd037dc2234043bc06570d0f39d20537
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-12-21 15:16:42 +0530

    adding echo
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,23 @@ int main() {
     if (input == "exit") {
       break;
     }
+    stringstream ss(input);
+    vector<string> tokens;
+    string token;
+    while (ss >> token) {
+      tokens.push_back(token);
+    }
+    if (tokens.empty()) {
+      continue;
+    }
+    if(tokens[0]=="echo"){
+      for(size_t i=1;i<tokens.size();i++){
+        cout<<tokens[i];
+        if(i!=tokens.size()-1)cout<<" ";
+      }
+      cout<<endl;
+      continue;
+    }
     cout << input << ": command not found" << endl;
 
   }
