0f0fbc70a9ec5062f0cd320b9da91802a4a4f9d5
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2026-01-06 23:55:05 +0530

    fix: improve tab completion handling and ensure proper reset of tab count
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- src/bindings/keys/tab.cpp
+++ src/bindings/keys/tab.cpp
@@ -2,12 +2,16 @@
 
 int on_tab(int count, int key)
 {
+    static int tab_cnt = 0;
     string line(rl_line_buffer);
+    if (line.empty())
+        return 0;
     int cursor = rl_point;
     vs res = autocomplete(line);
     if (res.size() == 0)
     {
-        cout << '\x07';
+        write(STDOUT_FILENO, "\a", 1);
+        tab_cnt = 0;
         return 0;
     }
     else if (res.size() == 1)
@@ -15,14 +19,25 @@ int on_tab(int count, int key)
         res[0] += " ";
         rl_replace_line(res[0].c_str(), 0);
         rl_point = rl_end;
+        tab_cnt = 0;
     }
     else
     {
-        cout << endl;
-        print(res);
-        rl_on_new_line();
-        rl_replace_line(line.c_str(), 0);
-        rl_point = cursor;
+        tab_cnt++;
+        if (tab_cnt == 1)
+        {
+            write(STDOUT_FILENO, "\a", 1);
+        }
+        else
+        {
+            sort(all(res));
+            cout << endl;
+            print(res);
+            rl_on_new_line();
+            rl_replace_line(line.c_str(), 0);
+            rl_point = cursor;
+            tab_cnt = 0;
+        }
     }
     rl_redisplay();
     return 0;
