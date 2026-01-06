c921ce7abb4d57bd060ecfd3b5dcf0b35fac7a73
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2026-01-06 22:16:53 +0530

    fix: improve autocomplete function by checking for null prefixNode before fetching matches
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- src/bindings/keys/autocomplete.cpp
+++ src/bindings/keys/autocomplete.cpp
@@ -5,6 +5,8 @@ vs autocomplete(string input)
     Trie *trie = new Trie();
     trie->insertArray(shell_builtin_commands);
     vs res;
-    trie->get_prefix_matches(input, res, input, trie->getPrefixNode(input));
+    Node *prefixNode = trie->getPrefixNode(input);
+    if (prefixNode != nullptr)
+        trie->get_prefix_matches(input, res, input, prefixNode);
     return res;
 }
