86ad06f092141b89e137298902ec7be36a122ee1
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2026-01-08 00:12:50 +0530

    refactor: consolidate handle_update logic for input navigation
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- src/bindings/keys/navigation.cpp
+++ src/bindings/keys/navigation.cpp
@@ -1,26 +1,25 @@
 #include "../../headers/commands.hpp"
 
-int previous_input(int count, int key)
+void handle_update()
 {
-    dec_history_idx();
     string to_replace = get_history_idx();
     if (!to_replace.empty())
     {
         rl_replace_line(to_replace.c_str(), 0);
         rl_redisplay();
         rl_point = rl_end;
-        }
+    }
+}
+int previous_input(int count, int key)
+{
+    dec_history_idx();
+    handle_update();
     return 0;
 }
 
 int next_input(int count, int key)
 {
     inc_history_idx();
-    string to_replace = get_history_idx();
-    if (!to_replace.empty())
-    {
-        rl_replace_line(to_replace.c_str(), 0);
-        rl_redisplay();
-    }
+    handle_update();
     return 0;
 }
